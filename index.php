5e228e3d28424720be3643ea18ed91252249eec7
Michael V <evesmikel@gmail.com>
2018-09-10 19:03:24 -0500

    Beautify form with css

--- index.php
+++ index.php
@@ -32,13 +32,26 @@ if($_POST['submit']) {
 <html>
 <head>
 	<title>PHP-SQL Login</title>
+<link rel="stylesheet" type="text/css" href="form.css">
 </head>
 <body>
+<div id="login-box">
+  <div class="left">
 <h1>Login</h1>
 <form method="post" action="index.php">
-	<input type="text" name = "username" placeholder="Enter username">
-	<input type="password" name="password" placeholder="Enter password here">
+	<input type="text" name = "username" placeholder="Enter username" />
+	<input type="password" name="password" placeholder="Enter password here" />
 	<input type="submit" name="submit" value="Login">
+	</div>
+<div class="right">
+    <span class="loginwith">Sign in with<br />social network</span>
+    
+    <button class="social-signin facebook">Log in with facebook</button>
+    <button class="social-signin twitter">Log in with Twitter</button>
+    <button class="social-signin google">Log in with Google+</button>
+  </div>
+  <div class="or">OR</div>
+</div>
 </form>
 
 <a href="register.php" >Register</a>
